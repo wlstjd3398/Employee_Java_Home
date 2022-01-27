873a3a6f51a3eeca217fd1bc981f98b487f4aca8
wlstjd3398 <wlstjd3398@naver.com>
2022-01-28 00:18:38 +0900

    2022-01-24

--- 2022-01-24/src/chapter02/Sol21.java
+++ 2022-01-24/src/chapter02/Sol21.java
@@ -8,11 +8,14 @@ public class Sol21 {
 		String strValue = "A";
 		
 		double var1 = (double) intValue; 
+//		업캐스팅 정수 -> 실수
 		byte var2 = (byte) intValue; 
+//		바이트가 작은것이니 다운캐스팅 정수 -> 정수?
 		int var3 = (int) doubleValue; 
+//		다운캐스팅 실수 ->정수
 		char var4 = (char) strValue; 
+//		다운캐스팅 문자열 -> 문자 
 
-		
-		
+//		문자열을 문자로 다운캐스팅 불가능?
 	}
 }
