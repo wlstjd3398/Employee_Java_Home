542896e04708f979b1ae78de7a060b8e1f07b88b
wlstjd3398 <wlstjd3398@naver.com>
2022-02-08 23:56:29 +0900

    2022-01-24

---
+++ 2022-01-24/src/chapter07/Ex2.java
@@ -0,0 +1,22 @@
+package chapter07;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Object1 obj1 = new Object1();
+		Object1 obj2 = new Object1();
+		
+		Car myCar = new Car();
+		Car yourCar = new Car();
+		
+		System.out.println(obj1);
+		System.out.println(obj2);
+		
+		// new라는 연산자가 사용되면 Object1, Car 인스턴스가 새로운 메모리주소를 가짐
+		
+	}
+
+}
---
+++ 2022-01-24/src/chapter07/Phone.java
@@ -0,0 +1,19 @@
+package chapter07;
+
+public class Phone {
+
+	// Phone이라는 클래스안에
+	
+	// 데이터타입이 Phone인 객체 안에는
+	// 데이터타입이 String maker 변수가 들어있고
+	// 데이터타입이 int인 price 변수가 들어있고
+	// 데이터타입이 double인 displaySize 변수가 들어있고
+	// 데이터타입이 int인 ram 변수가 들어있다
+	
+	
+	String maker;
+	int price;
+	double displaySize;
+	int ram;
+	
+}
