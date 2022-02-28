4bd1d50d244654ee58c4ba8332e6546df8e88ae7
wlstjd3398 <wlstjd3398@naver.com>
2022-02-28 15:53:40 +0900

    2022-01-24

--- 2022-01-24/src/chapter17/Ex9.java
+++ 2022-01-24/src/chapter17/Ex9.java
@@ -23,6 +23,30 @@ public class Ex9 {
 		System.out.println(list.contains(c2)); // 홍길동의 정보가 이 리스트에 들어있나요?
 		// contains에 for반복문이 들어있음(ppt확인)
 		
+		
+		
+//		list.clear(); // 클리어를 통해 비워짐
+//		System.out.println(list.isEmpty()); // 그래서 비워짐 -> true
+		
+		
+		System.out.println("----- ----- -----");
+		Customer removeCustomer = new Customer("고영희", "010-1111-2222", 2);
+		System.out.println("삭제 전 -> " + list);
+		
+		list.remove(removeCustomer);
+		System.out.println("삭제 후 -> " + list);
+		
+		
+		if(list.remove(removeCustomer)) { 
+			// 지운결과를 다시 지우니 지울 데이터가 list없다고 뜸
+			System.out.println("데이터를 list에서 지웠습니다.");
+		}else {
+			System.out.println("지울 데이터가 list에 없습니다.");
+		}
+		
+		
+		
+		
 	}
 
 }
