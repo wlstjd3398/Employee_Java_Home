bf0c2885091087aa674c8c2a456f92e00317b89d
wlstjd3398 <wlstjd3398@naver.com>
2022-03-16 21:49:06 +0900

    포켓몬빵 당첨 작성중

--- 2022-01-24/src/project01/pokemon1.java
+++ 2022-01-24/src/project01/pokemon1.java
@@ -14,22 +14,27 @@ public class pokemon1 {
 		
 		// 2.추첨권확인
 		// 3.진행가능여부멘트
-		System.out.println("해당 스티커의 추첨번호를 입력해주세요");
-		Scanner scan = new Scanner(System.in);
-		String draw = scan.next();
 		
-		int num1 = draw.length();
-		// 추첨권 확인 안되면 -> 다음 페이지 진입불가
 		
-		// 추첨권 자리수확인ok, 추첨권이 가짜확인 아직
-		if(num1<=10 && num1>=0) {
-			System.out.println("올바른 추첨번호입니다. 경품을 선택해 응모해주세요.");
-			// 다음 페이지
+		while(true) {
+			System.out.println("해당 스티커의 추첨번호를 입력해주세요");
+			Scanner scan = new Scanner(System.in);
+			String draw = scan.next();
 			
-		}else {
-			System.out.println("해당 추첨번호가 일치하지 않습니다.");
+			int num1 = draw.length();
+			// 추첨권 확인 안되면 -> 다음 페이지 진입불가 -> html 화면구현(jsp?)
+			
+			// 추첨권 자리수확인ok, 추첨권이 가짜확인 아직
+			if(num1<=10 && num1>=0) {
+				System.out.println("= = = = = = = = = = = =");
+				System.out.println("올바른 추첨번호입니다. 경품을 선택해 응모해주세요.");
+				// 다음 페이지 -> html 화면구현(jsp?)
+				break;
+			}else {
+				System.out.println("해당 추첨번호가 일치하지 않습니다. 다시 입력해주세요.");
+			}
 			// 추첨번호입력 블록 커서 재이동
-			
+			continue;
 		}
 		
 		
--- 2022-01-24/src/project01/pokemon2.java
+++ 2022-01-24/src/project01/pokemon2.java
@@ -12,15 +12,31 @@ public class pokemon2 {
 		// 5.경품체크박스 -> html 화면구현
 		
 		// 6.응모하기버튼	
-		Scanner scan = new Scanner(System.in);
-		int enter = scan.nextInt(5);
-		
-		if(enter==1 | enter==2 | enter==3 | enter==4) {
-			System.out.println("응모되었습니다. 결과보기를 눌러 날짜를 확인해주세요");	
-		}else {
-			System.out.println("버튼을 하나 선택해 응모해주세요");
+		while(true) {
+			System.out.println("=====추첨번호 확인 후 응모 시작=====");
+			Scanner scan = new Scanner(System.in);
+			int enter = scan.nextInt(5);
+			
+			if(enter==1) {
+				System.out.println("응모되었습니다. 결과보기를 눌러 날짜를 확인해주세요");
+				break;
+			}else if(enter==2){
+				System.out.println("응모되었습니다. 결과보기를 눌러 날짜를 확인해주세요");
+				break;
+			}else if(enter==3) {
+				System.out.println("응모되었습니다. 결과보기를 눌러 날짜를 확인해주세요");
+				break;
+			}else if(enter==4) {
+				System.out.println("응모되었습니다. 결과보기를 눌러 날짜를 확인해주세요");
+				break;
+			}else if(enter==0){
+				System.out.println("버튼을 하나 선택해 다시 응모해주세요");
+			}else {
+				System.out.println("버튼을 하나 선택해 다시 응모해주세요");
+			}
+			// 선택하지 않거나 중복 선택시 다시 응모하여 하나만 선택응모해주세요
+			continue;
 		}
-		
 		
 		
 	}
