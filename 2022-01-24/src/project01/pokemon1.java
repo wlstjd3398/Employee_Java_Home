de5178bc36849c39ce4eb7608e280042f2aac9bc
wlstjd3398 <wlstjd3398@naver.com>
2022-03-22 02:43:47 +0900

    수정 중

--- 2022-01-24/src/project01/pokemon1.java
+++ 2022-01-24/src/project01/pokemon1.java
@@ -23,56 +23,42 @@ public class pokemon1 {
 		
 		// while반복문 없어도?
 		while(true) {
-			System.out.println("스티커의 추첨번호를 입력해주세요");
-			String[] arr1 = new String[2];
+		
+			System.out.println("빵의 스티커 뒷면의 추첨번호를 입력해주세요");
 			
-			Scanner scan1 = new Scanner(System.in);
-			String draw = scan1.next();
-			int num1 = draw.length();
+			Scanner rightScan = new Scanner(System.in);
+			String drawScan = rightScan.next();
+			int limit = drawScan.length();
 			
-			if(num1<=10 && num1>=8) {
+			System.out.println("추첨번호를 확인하겠습니다.");
+			
+			// 추첨번호를 목록에 차례대로 저장시켜서 노출시켜야함
+			
+			if(limit<=10 && limit>=8) {
 				System.out.println("올바른 추첨번호입니다. 경품을 선택해 응모해주세요.");
+					String[] arr1 = new String[2];			
+					for(int i=0; i<arr1.length; i++) {
+						arr1[i] = drawScan;
+						System.out.println("저장된 추첨목록의 추첨번호 = " + arr1[i]);
+					}
 				break;
 			}else {
 				System.out.println("해당 추첨번호가 일치하지 않습니다. 다시 입력해주세요.");
 			}
-			
-			for(int i=0; i<arr1.length; i++) {
-//				System.out.println("추첨번호를 입력하세요.");
-				arr1[i] = scan1.next();
-
-				System.out.println("추첨번호를 확인하겠습니다.");
-			}
-			
-			
-//			String draw = scan1.next();
-//			
-//			int num1 = draw.length();
-			
-//			if(num1==8) {
-//				System.out.println("올바른 추첨번호입니다. 경품을 선택해 응모해주세요.");
-//				break;
-//			}else {
-//				System.out.println("해당 추첨번호가 일치하지 않습니다. 다시 입력해주세요.");
-//			}
-			
-			
-			for(int i=0; i<arr1.length; i++) {
-				System.out.println("저장된 추첨목록의 추첨번호 = " + arr1[i]);
-			}
-			
 		}
-
+		
+		
+		System.out.println("= = = = = 응모를 시작합니다. = = = = =");
+		
 		
 		while(true) {
-			System.out.println("= = = = = 응모를 시작합니다. = = = = =");
 			System.out.println("4가지 경품 중 한가지를 번호를 선택하여 응모해주세요");
 			System.out.println("경품 1번(닌텐도 NDS)");
 			System.out.println("경품 2번(포켓몬 완구)");
 			System.out.println("경품 3번(포켓몬 영화관람권)");
 			System.out.println("경품 4번(포켓몬 스티커 컬렉션 북)");
-			Scanner scan2 = new Scanner(System.in);
-			int enter = scan2.nextInt(5);
+			Scanner enterScan = new Scanner(System.in);
+			int enter = enterScan.nextInt(5);
 			
 			if(enter==1) {
 				System.out.println("경품 1번(닌텐도 NDS)에 응모되었습니다. 결과보기를 눌러 당첨여부를 확인해주세요");
@@ -94,24 +80,27 @@ public class pokemon1 {
 			
 		}
 		
-
+		
 		System.out.println("= = = = = 결과보기 = = = = =");
 		
+		
         if(true) {
-    		System.out.println("당신의 당첨여부는?");
+        	System.out.println("과연");
+    		System.out.println("당첨여부는? ");
     		Random random1 = new Random();
     		int yn = random1.nextInt(2);
     		 if(yn==1) {
     			 System.out.println("당첨입니다. 해당 휴대폰번호로 안내문자가"
     			 		+ " 오늘 중으로 갈예정입니다. 그러니 휴대폰번호를 입력해주세요.");
+    			 
     			// 휴대폰번호
     			while(true) {
     				System.out.println("-를 포함한 13자리를 입력해주세요.");
-    				Scanner scan3 = new Scanner(System.in);
-    				String phoneNumber = scan3.next();
+    				Scanner PhoneScan = new Scanner(System.in);
+    				String phoneNumber = PhoneScan.next();
     				
-    				int num1 = phoneNumber.length();
-    				if(num1==13) {
+    				int num3 = phoneNumber.length();
+    				if(num3==13) {
     					System.out.println("올바른 휴대폰번호입니다. 완료되었습니다.");
     					break;
     				}else {
