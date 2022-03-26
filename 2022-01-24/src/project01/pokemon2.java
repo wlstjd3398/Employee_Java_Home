38c2333eb09ded6bf2131e518e020e8633cd576e
wlstjd3398 <wlstjd3398@naver.com>
2022-03-27 04:17:09 +0900

    수정

--- 2022-01-24/src/project01/pokemon2.java
+++ 2022-01-24/src/project01/pokemon2.java
@@ -22,41 +22,82 @@ public class pokemon2 {
 // 학원에서 수정한것 참고하고 java는 배열은 잘쓰지않고 list 잘씀
 // chapter17_2,3 컬렉션프레임워크list(1),(2)참고
 // list로 해서 다 추첨하고나서 휴대폰번호는 따로 받기
-		List<String> strList = new ArrayList<>(2);
-		
-		int strListSize = strList.size();
-		
+//		List<String> strList = new ArrayList<>();
+//		
+//		int strListSize = strList.size();
+//		
+//
+//		System.out.println("빵의 스티커 뒷면의 추첨번호를 입력해주세요");
+//
+//		//chapter17 -> Ex4, Ex3 참고
+//		
+//			for(int i=0; i<=strListSize; i++) {
+//				
+//				Scanner drawScan = new Scanner(System.in);
+//				
+//				String draw = drawScan.next();
+//				
+//				//여기서부터 수정해야함
+//				strList.add(draw);
+//				
+//				strList.set(i, draw);
+//				
+//				int limit = draw.length();
+//				
+//				System.out.println("추첨번호를 확인하겠습니다.");
+//				
+//				if(limit<=10 && limit>=8) {
+//					System.out.println("확인된 추첨목록의 추첨번호 = " + strList);
+//					
+//				}else {
+//					System.out.println("추첨번호를 다시 입력해주세요");
+//					i--;
+//				}
+//				
+//			}
+//
+//		
+//		for(int i=0; i<=strListSize; i++) {
+//			System.out.println("추첨목록에 저장된 추첨번호 = " + strList);
+//		}
 
-		System.out.println("빵의 스티커 뒷면의 추첨번호를 입력해주세요");
 		
-		Scanner drawScan = new Scanner(System.in);
 		
-		//chapter17 -> Ex4, Ex3 참고
 		
-			for(int i=0; i<strListSize; i++) {
+		System.out.println("추첨인원은 몇명입니까?");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		
+		ArrayList<String> strList = new ArrayList<String>();
+		
+		
+		for(int i=0;i<num;i++) {
+			
+			System.out.println("추첨번호를 입력하세요");
+			
+			int limit = sc.next().length();
+			
+			if(limit <= 10 && limit >= 8) {
+				//추첨번호를 바로 위에서 한번, 여기서 한번 또 입력하게함
+				//입력하지 않아도 바로 위에서 입력한 값을 바로 추가할수있게해야함
+				strList.add(sc.next());  
 				
-				
-				arr1[i] = drawScan.next();
-				
-				int limit = arr1[i].length();
-				
-				System.out.println("추첨번호를 확인하겠습니다.");
-				
-				if(limit<=10 && limit>=8) {
-					System.out.println("확인된 추첨목록의 추첨번호 = " + strList);
-					
-				}else {
-					System.out.println("추첨번호를 다시 입력해주세요");
-					i--;
-				}
-				
+			}else {
+				i--;
 			}
-
-		
-		for(int i=0; i<strListSize; i++) {
-			System.out.println("추첨목록에 저장된 추첨번호 = " + strList);
+			
+			
+			
+			
 		}
-
+		System.out.println(strList);
+		
+		System.out.println("완료");
+	
+		
 		
 		
 		
