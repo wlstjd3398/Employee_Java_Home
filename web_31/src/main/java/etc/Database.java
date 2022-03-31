714b81fd375aa367d53543f9247b2f3a185eaa84
wlstjd3398 <wlstjd3398@naver.com>
2022-04-01 02:46:46 +0900

    미니프로젝트

---
+++ web_31/src/main/java/etc/Database.java
@@ -0,0 +1,13 @@
+package etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vo.MemberInfo;
+
+public class Database {
+	public static List<MemberInfo> memberInfoTable = new ArrayList<>();
+	//클래스의 정보가 읽힐때 static 사용
+	
+	
+}
