71c880b2cadc049b80eedb9703365de5a4a8a3d3
wlstjd3398 <wlstjd3398@naver.com>
2022-04-17 01:43:17 +0900

    DB연동, 리팩토링, update, delete

---
+++ web_31/src/main/java/etc/MenuNumber.java
@@ -0,0 +1,18 @@
+package etc;
+
+public class MenuNumber {
+
+	//나중에 보면 swtich case1이 무언인지 알기 어려움
+	
+	// 클래스 멤버 변수를 활용해서 1번 메뉴의 이름을 생성
+	public static final int JOIN = 1;
+	// 2번 메뉴 이름은 로그인
+	public static final int LOGIN = 2;
+	// 5번 메뉴 이름은 종료
+	public static final int EXIT = 5;
+	// 3번 메뉴 이름은 수정
+	public static final int UPDATE = 3;
+	// 4번 메뉴 이름은 탈퇴
+	public static final int DELETE = 4;
+	
+}
