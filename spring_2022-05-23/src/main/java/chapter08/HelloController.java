8fe99207cdcdc3ea73a9db202ed2aed2d9636d85
wlstjd3398 <wlstjd3398@naver.com>
2022-06-10 02:58:02 +0900

    스프링 MVC 프레임워크 동작방식

--- spring_2022-05-23/src/main/java/chapter08/HelloController.java
+++ spring_2022-05-23/src/main/java/chapter08/HelloController.java
@@ -16,6 +16,14 @@ public class HelloController {
 			@RequestParam(value = "name", required=false) String name) {
 //		System.out.println("Hello Controller 의 hello() 메서드 호출");
 		
+		// 이 메서드가 반환한 값은 HandlerAdapter를 통해 ModelAndView로 변환되서 전달됨
+		
+		// 아래와 같이 Model 매개변수에 값을 저장하면 
+		// 뷰(hello.jsp) 가 전달 받게 되고
+		// 모델을 전달 받은 뷰는 request.setAttribute("greeting", name + " 님 안녕하세요~!"); 로 변환해서
+		// request 영역에 저장함
+		// 그래서 뷰에서는 Model에 담겨있는 값을 request 영역 안에서 꺼내 쓸 수 있음
+		// (컨트롤러가 전달한 값을 뷰에서 꺼내서 쓸 수 있음)
 		model.addAttribute("greeting", name + " 님 안녕하세요~!");
 		
 		return "hello";
--- spring_2022-05-23/src/main/java/chapter08/MvcConfig.java
+++ spring_2022-05-23/src/main/java/chapter08/MvcConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 // 이 설정 클래스가 가지고 있는 스프링 MVC 활성화하는 애노테이션
+// 낮은버전 mvc는 수백줄의 코드를 bean등록해줘야함
+// 높은버전 mvc는 애노테이션 하나로 대신 할 수 있음
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer{
 
@@ -16,6 +18,10 @@ public class MvcConfig implements WebMvcConfigurer{
 	
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// JSP를 이용해서 컨트롤러의 처리 결과를 클라이언트에게 전달하기 위해서 이 메서드를 오버라이딩했음
+		
+		// jsp() 메서드 : 컨트롤러의 처리 결과를(접두사, 접미사) 형태의 JSP로 전달하겠다라는 메서드
+		
 		// view가 눈에 보이는 접두사와 접미사를 붙여서 클라이언트에게 전달하도록 함 
 		registry.jsp("/WEB-INF/view/", ".jsp");
 	}
