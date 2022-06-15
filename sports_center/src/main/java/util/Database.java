9417df5e75b746b0e1165e43048aba7f4680bd1f
wlstjd3398 <wlstjd3398@naver.com>
2022-06-16 04:17:37 +0900

    포트폴리오

--- sports_center/src/main/java/util/Database.java
+++ sports_center/src/main/java/util/Database.java
@@ -1,7 +1,73 @@
 package util;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import vo.MemberInfo;
 
+public class Database {
+	// DB 커넥션을 생성해서 반환하는 메서드
+	public Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:8080/sports_center?user=root&password=1234");
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn; 
+		// try에서 예외가 발생안했을때 conn을 반환
+		
+	}
 	
+	
+	// 메서드만 호출하면 매번 stmt, conn을 닫아줄 필요가 없음
+	public void closeStmt(Statement stmt) {
+		if(stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void closePstmt(PreparedStatement pstmt) {
+		if(pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void closeConnection(Connection conn) {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
